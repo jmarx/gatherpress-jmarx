ff044fcac68836c0c157860843afa393872e0ac8
Jeffrey Marx <jeffrey_marx@yahoo.com>
2023-01-25 19:59:39 -0500

    Add new UI for maps

--- build/blocks/venue-information/index.asset.php
+++ build/blocks/venue-information/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'bfe83fe01021c35826ed');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b2326223c934f8b66dbf');
